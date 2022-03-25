55fb5c04f48261c7969206ddfe12e71974468af9
Adiani <myrzaevaa19@gmail.com>
2022-03-25 16:53:22 +0200

    управление от 1-го лица

---
+++ TVD.Unity/Assets/Scripts/FPCController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPCController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CharacterController characterController;
+    public GameObject _camera;
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 verticalValue = transform.forward * Input.GetAxis("Vertical");
+        Vector3 horizontalValue = transform.right * Input.GetAxis("Horizontal");
+        Vector3 totalValue = horizontalValue + verticalValue;
+        characterController.SimpleMove(totalValue);
+        float turnSpeed = 5.0f;
+        float rotateValue = Input.GetAxis("Mouse X") * turnSpeed;
+        float rotatezValue = Input.GetAxis("Mouse Y");
+        transform.Rotate(0, rotateValue, 0);
+        _camera.transform.Rotate(rotatezValue, 0, 0);
+    }
+}
