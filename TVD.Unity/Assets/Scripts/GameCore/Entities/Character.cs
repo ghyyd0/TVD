db8db54a992498af371a5eded947ee570ccdae62
alkihuri <alkihuri@gmail.com>
2022-03-26 16:33:08 +0200

    Character and Human class  definition

---
+++ TVD.Unity/Assets/Scripts/GameCore/Entities/Character.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum CharacterType
+{
+    human,
+    vampire,
+    werewolf
+}
+public class Character : MonoBehaviour
+{
+    public CharacterType type;
+    public float hp;
+    public float speed;
+    public List<CharacterType> keysType = new List<CharacterType>(); 
+
+}
---
+++ TVD.Unity/Assets/Scripts/GameCore/Entities/Human.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : Character
+{
+    private void Awake()
+    {
+        type = CharacterType.human;
+        speed = 5;
+        keysType.Add(CharacterType.vampire); 
+        keysType.Add(CharacterType.werewolf);
+    }
+}
