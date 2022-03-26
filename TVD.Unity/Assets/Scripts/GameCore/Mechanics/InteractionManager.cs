9d9c3b6725a5d68f04bb3087034ba37824ccf52f
Adiani <myrzaevaa19@gmail.com>
2022-03-26 17:06:13 +0200

    RayCast settings

---
+++ TVD.Unity/Assets/Scripts/GameCore/Mechanics/InteractionManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    RaycastHit objectOnHitLine;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out objectOnHitLine))
+        {
+
+        }
+    }
+}
